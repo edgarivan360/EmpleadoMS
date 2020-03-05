cbea56637beca5f95c351c9fdac7a0aaab314e92
Edgar Quiroz <edgar_qp11@outlook.com>
2020-03-05 12:30:51 -0600

    Creacion SpringBootApplication y agregación de dependencias necesarias

---
+++ src/main/java/com/wombats/empleado/EmpleadoMs.java
@@ -0,0 +1,19 @@
+package com.wombats.empleado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("com.wombats.empleado")
+@EnableJpaRepositories({"com.wombats.empleado.persistence.repositories"})
+@EntityScan({"com.wombats.empleado.model"})
+public class EmpleadoMs extends SpringBootServletInitializer {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(EmpleadoMs.class, args);
+    }
+}
